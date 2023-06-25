7ef12aa4a33aa7c0e81b7294d914b36da00ebdfa
Abdulrahman-Alkhalaileh <abdulrahman.alkhalaileh@gmail.com>
2023-06-25 23:09:43 +0300

    add more information in about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -8,7 +8,11 @@ const AboutPage = () => {
         <h2>About this project</h2>
         <h4>This is a React app to leave a feedback for a service or a product</h4>
         <p><strong>Created by: </strong>Abdulrahman Alkhalaileh</p>
-        <Link to='/'>Back to Home</Link>
+        <p className='about-paragraph'>To see the source code of this project, go to my <b>
+                <a rel="noreferrer" target="_blank"
+                href='https://github.com/Abdulrahman-Alkhalaileh/feedback-ui'
+                >GitHub repository</a></b></p>
+        <p className='back-to-home'><Link to='/' ><b>Back to Home</b></Link></p>
     </Card>
   )
 }
